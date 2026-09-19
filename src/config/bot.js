17c91fcdb127a603fb16e3c97ab0ba1ba5f87403
legendzworks-ops <legendzworks@gmail.com>
2026-09-19 15:04:45 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "Watching Khaos Reapers", // required by Discord API, not shown in the client
         state: "Khaos Reapers",     // this is what people actually see
-        type: 3,               // Watching
+        type: 4,               // Custom
       },
     ],
   },
