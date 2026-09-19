639aa2cad9b3f847425f112830dc545998cdad0a
legendzworks-ops <legendzworks@gmail.com>
2026-09-19 13:35:36 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Watching Status", // required by Discord API, not shown in the client
         state: "Khaos Reapers",     // this is what people actually see
         type: 3,               // Custom
       },
